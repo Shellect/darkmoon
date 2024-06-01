2974a39ecd95f133e71ad5fbee715847aa54f22c
Aren <a.vardanyan00101@gmail.com>
2024-06-02 00:48:25 +0300

    add a lot of things

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
 const swiper = new Swiper('.swiper', {
    loop: true,
+   slidesPerView: 1,
    pagination: {
       el: '.swiper-pagination',
    },
-   navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-   },
-   slidesPerView: 3,
-   spaceBetween: 67
+   breakpoints: {
+      500: {
+         // loop: true,
+         // pagination: {
+         //    el: '.swiper-pagination',
+         // },
+         navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+         },
+         slidesPerView: 3,
+         spaceBetween: 67
+      }
+   }
 });
+
+
